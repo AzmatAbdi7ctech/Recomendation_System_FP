20f1b1ef4e96ce42b37f909a0a0828ac92b69c88
AzmatAbdi7ctech <azmatali7ctech@gmail.com>
2024-01-12 18:25:07 +0500

    added packages

--- Code/utils/libraries.py
+++ Code/utils/libraries.py
@@ -9,3 +9,19 @@ from pyspark.sql.functions import date_format
 import os
 from dotenv import load_dotenv
 load_dotenv()
+import re
+import nltk
+from nltk.stem import PorterStemmer
+from nltk.corpus import stopwords
+
+from nltk.stem import WordNetLemmatizer
+
+import sklearn as sk
+import numpy as np
+import re
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sentence_transformers import SentenceTransformer, util
+# nltk.download('punkt')
+# nltk.download('stopwords')
+# nltk.download('wordnet')
