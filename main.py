d4ff0e178aae037bbf551802f8799b2ee6a57265
AzmatAbdi7ctech <azmatali7ctech@gmail.com>
2024-01-29 16:39:31 +0500

    main changed

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from Code.data.dataprocessing.extract.collection_category_tag import *
 from Code.data.dataprocessing.extract.tag_cloud  import *
 from sessions import *
 from pyspark.sql.functions import date_format
-
+from Code.utils.logs import *
 BASE_PATH=os.getenv('BASEPATH')
 CONN_URL = os.getenv('CONN_URL')
 PASSWORD = os.getenv('PASSWORD')
@@ -16,50 +16,58 @@ USERNAME=os.getenv('USER_NAME')
 configur = ConfigParser() 
 configur.read(BASE_PATH+'config.ini')
 spark_session=session()
-#spark_driver=configur.get('driver_path_pyspark','pyspark')
-
+# spark_driver=configur.get('driver_path_pyspark','pyspark')
 try:
         dump_csv(product_transformation
                 (
                 extract_data(spark=spark_session,table_name=configur.get('table_extract','product'),
                 password=PASSWORD,user_name=USERNAME,fetech_size=configur.get('size','fetch_size'),conn_url=CONN_URL),
-                column_list=['product_id', 'product_title', 'published_at', 'product_thumbnail', 'created_at', 'updated_at'])
+                column_list=['product_id', 'product_title'])
                 ,file_path=configur.get('BasePath','Path'),table_name=configur.get('table_extract','product')
                 )
+        logging.info('--product table extracted--')
 except:
-        print('product not extracted')
+        logging.debug('--product table not extracted--')
+
 
 try:
         dump_csv(producttag_transformation
         (
         extract_data(spark=spark_session,table_name=configur.get('table_extract','product_tag'),
         password=PASSWORD,user_name=USERNAME,fetech_size=configur.get('size','fetch_size'),
-        conn_url=CONN_URL)
+        conn_url=CONN_URL,column_list=['product_id','tag_id'])
         ),file_path=configur.get('BasePath','Path'),table_name=configur.get('table_extract','product_tag')
         )
+        logging.info('--product tags table extracted--')
 except:
-        print("product tag not extracted")
-try:
+        logging.debug('--product tags table not extracted--')
+
+
+try:        
         dump_csv(collection_category_transformation
                 (extract_data(spark=spark_session,table_name=configur.get('table_extract','collection_category'),
                         password=PASSWORD,user_name=USERNAME,fetech_size=configur.get('size','fetch_size'),conn_url=CONN_URL)
                 ,column_list=['id','name','parent_id']),file_path=configur.get('BasePath','Path'),table_name=configur.get('table_extract','collection_category'))
-
+        logging.info('--collection category table extracted--')
 except: 
-        print('collection  category not extracted')
+        logging.debug('--collection category table not extracted--')
+
 
 try:
         dump_csv(collection_category_tag_transformation
                 (extract_data(spark=spark_session,table_name=configur.get('table_extract','collection_category_tag'),
                          password=PASSWORD,user_name=USERNAME,fetech_size=configur.get('size','fetch_size'),conn_url=CONN_URL)
                 ,column_list=['id','category_id','tag_id']),file_path=configur.get('BasePath','Path'),table_name=configur.get('table_extract','collection_category_tag'))
+        logging.info('--collection category tag table--')        
 except:
-        print('collection category tag not extracted')
+        logging.info('--collection category tag not extracted--')
+
 
 try:
         dump_csv(tag_cloud_transformation
                  (extract_data(spark=spark_session,table_name=configur.get('table_extract','tag_cloud'),
                          password=PASSWORD,user_name=USERNAME,fetech_size=configur.get('size','fetch_size'),conn_url=CONN_URL),
                  column_list= ['id', 'tag_name']),file_path=configur.get('BasePath','Path'),table_name=configur.get('table_extract','tag_cloud'))
+        logging.info('--tag_cloud table extracted--')
 except:
-        print('tag cloud is not extracted')
+        logging.info('--tag cloud table is not extracted--')
